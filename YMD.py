8f5d4458792520de0f945a19b862a5843b58c5e3
Ravi Kant <irvhny@gmail.com>
2023-01-09 14:01:08 +0530

    second commit

---
+++ YMD.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+import os
+
+now = datetime.now()
+year = int(now.strftime('%Y'))
+month_int = (now.strftime('%m').zfill(2))
+day = now.strftime('%d')
+date_str = f'{year}{month_int}{day}'
+print(date_str)
+
+os.mkdir(date_str)
